686ecbd637698ad9e8872f21fc3721324b2be20a
Moyo Williams <mow5248@psu.edu>
2013-05-13 10:28:09 -0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//---------------------------------------------------- 
+// Name: Moyo Williams
+// E-mail Address: mow5248@psu.edu
+// Class: CMPSC 122, Section 102
+// Final Project
+// Date:04/22/2013
+// Hangman Console Game
+//----------------------------------------------------
+
+#include <iostream>
+#include <string>
+#include "HangMan.h"
+
+using namespace std;
+
+
+
+
+int main()
+{
+    HangMan hangman; //hangman object
+  hangman.StartGame();   //runs our game
+    return 0;
+}
+
